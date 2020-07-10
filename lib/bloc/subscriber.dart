fbfa4b0aab283c218bc6ee2c65236a48e901085a
piyushchauhan <pi.codemonk@gmail.com>
2020-07-10 18:00:04 +0530

    Connected slots to subscriber bloc

--- lib/bloc/subscriber.dart
+++ lib/bloc/subscriber.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:developer';
 
 import 'package:qme/api/base_helper.dart';
 import 'package:qme/model/queue.dart';
 import 'package:qme/model/reception.dart';
+import 'package:qme/model/user.dart';
 import 'package:qme/repository/appointment.dart';
 import 'package:qme/repository/queue.dart';
 
@@ -42,60 +42,26 @@ class SubscriberBloc {
   }
   fetchReceptions() async {
     receptionListSink.add(ApiResponse.loading('Fetching appointment slots'));
-    final response = jsonDecode('''{
-    "counters": [
-        {
-            "id": "FyKQeYVM8",
-            "subscriber_id": "17dY6K8Hb",
-            "starttime": "2020-06-28T20:00:00.000Z",
-            "endtime": "2020-06-28T21:00:00.000Z",
-            "slot": 15,
-            "cust_per_slot": 1,
-            "status": "UPCOMING"
-        },
-        {
-            "id": "pgXN_rw-0",
-            "subscriber_id": "17dY6K8Hb",
-            "starttime": "2020-06-28T18:00:00.000Z",
-            "endtime": "2020-06-28T19:45:00.000Z",
-            "slot": 15,
-            "cust_per_slot": 3,
-            "status": "UPCOMING"
-        },
-        {
-            "id": "pgXN_rw-0",
-            "subscriber_id": "17dY6K8Hb",
-            "starttime": "2020-06-28T18:00:00.000Z",
-            "endtime": "2020-06-28T19:45:00.000Z",
-            "slot": 15,
-            "cust_per_slot": 3,
-            "status": "UPCOMING"
-        }
-    ]
-}''');
 
-    List<Reception> _receptions = [];
-    for (var element in response['counters']) {
-      _receptions.add(Reception.fromJson(Map<String, dynamic>.from(element)));
-    }
-    receptionList = _receptions;
-    /*
-    String accessToken = await getAccessTokenFromStorage();
-    List<Reception> _receptions =
-        await _appointmentRepository.viewReceptionsByStatus(
-      subscriberId: subscriberId,
-      status: ['UPCOMING', 'ACTIVE'],
-      accessToken: accessToken,
-    );*/
+    try {
+      final String accessToken = await getAccessTokenFromStorage();
+      List<Reception> _receptions =
+          await _appointmentRepository.viewReceptionsByStatus(
+        subscriberId: subscriberId,
+        status: ['UPCOMING', 'ACTIVE'],
+        accessToken: accessToken,
+      );
+      // slots from start time and send and slot duration
+      for (int i = 0; i < receptionList.length; i++) {
+        Reception reception = receptionList[i];
+        reception.createSlots();
+      }
+      receptionList = _receptions;
 
-    // slots from start time and send and slot duration
-    for (int i = 0; i < receptionList.length; i++) {
-      Reception reception = receptionList[i];
-      reception.createSlots();
-      /*List<Slot> slots = createSlotsFromDuration(reception);
-      reception.addSlotList(slots);*/
+      receptionListSink.add(ApiResponse.completed(receptionList));
+    } on Exception catch (e) {
+      log('Error in SubscriberBLOC:fetchExceptions:${e.toString()}');
     }
-    receptionListSink.add(ApiResponse.completed(receptionList));
   }
 
   fetchQueuesList() async {
