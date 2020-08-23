6a40a9f0bf657fe31caa49685076cddb10542a6d
Yash Dixit <52655208+udzhsay@users.noreply.github.com>
2020-08-23 17:23:49 +0530

    Deleted one file, and awaited on all hive commands

--- lib/model/hive_appointment.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:hive/hive.dart';
-
-// @HiveType(typeId: 0)
-// class AppointmentBox extends HiveObject {
-//   @HiveField(0)
-//   String id;
-//   @HiveField(1)
-//   bool booked;
-// }
