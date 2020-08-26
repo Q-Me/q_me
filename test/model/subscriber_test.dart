35f0f5851002b495ec09e49eac33b5497afb6e4d
piyushchauhan <pi.codemonk@gmail.com>
2020-08-26 13:06:17 +0530

    Added test for subscriber model but not passing

--- test/model/subscriber_test.dart
+++ test/model/subscriber_test.dart
@@ -5,9 +5,8 @@ import 'package:test/test.dart';
 
 void main() {
   test('w/ json ...', () {
-    expect(
-        Subscriber.fromJson(
-          json.decode('''
+    final testSubscriber = Subscriber.fromJson(
+      json.decode(r'''
             {
             "id": "lJELQyMtS",
             "name": "S1",
@@ -25,22 +24,26 @@ void main() {
             "verified": 0,
             "displayImages": ["lJELQyMtS_1.jpeg","lJELQyMtS_2.png"]
           }'''),
-        ),
-        Subscriber(
-          id: "lJELQyMtS",
-          name: "S1",
-          description: "",
-          owner: "Kavya",
-          email: "Kavya24@gmail.com",
-          phone: "+918585992062",
-          address: "Patna Estate",
-          tags: ["ABC", "CDE"],
-          longitude: 85.594095,
-          latitude: 25.137566,
-          rating: 5,
-          category: "Saloon",
-          displayImages: ["lJELQyMtS_1.jpeg", "lJELQyMtS_2.png"],
-          verified: false,
-        ));
+    );
+    print(testSubscriber.toJson());
+    expect(
+      testSubscriber,
+      Subscriber(
+        id: "lJELQyMtS",
+        name: "S1",
+        description: "",
+        owner: "Kavya",
+        email: "Kavya24@gmail.com",
+        phone: "+918585992062",
+        address: "Patna Estate",
+        tags: List<String>.from(["ABC", "CDE"]),
+        longitude: 85.594095,
+        latitude: 25.137566,
+        rating: 5,
+        category: "Saloon",
+        displayImages: List<String>.from(["lJELQyMtS_1.jpeg", "lJELQyMtS_2.png"]),
+        verified: false,
+      ),
+    );
   });
 }
